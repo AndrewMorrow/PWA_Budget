4c04c2abedee4fedc77b4a2f32107bdc987c660d
AndrewMorrow <beardedmongo@gmail.com>
2021-01-27 16:39:14 -0600

    Updates with webpack and service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
+    "/styles.css",
     "/dist/app.bundle.js",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
